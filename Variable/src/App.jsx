d36b5ef944096352beee9d5d407bb9fd864dde2f
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-09-02 01:00:57 -0500

    change few style

--- Variable/src/App.jsx
+++ Variable/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './styles.css'
-
+import styles from './styles.css'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div> 
+        <header className={styles.header}>  
+          <h1>Welcome to GitHub</h1>
+        </header>
+        <main className={styles.main}>
+          <div className={styles.centralBox}></div>
+            
+    
+            <h1>Friday</h1>
+        </main>
+        <footer className={styles.footer}>
+          <p>This is end of the week</p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
